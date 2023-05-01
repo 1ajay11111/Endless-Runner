ec766efa0f35a3ebc21062aae97b5f2997a6215f
Ajay Bharat Bhalodiya <122375043+1ajay11111@users.noreply.github.com>
2023-05-01 16:12:03 +0530

    Update CameraController.cs

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -1,19 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-   
-    // Start is called before the first frame update
+    private Transform target;
+    private Vector3 offset;
+
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - target.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, offset.z + target.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition, 0.6f);
     }
 }
