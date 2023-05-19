e9b1847e57e0d8e11b63a132fe5529ff96004bd9
Ajay Bharat Bhalodiya <122375043+1ajay11111@users.noreply.github.com>
2023-05-19 11:57:59 +0530

    Update Coin.cs

--- Scripts/Coin.cs
+++ Scripts/Coin.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(50 * Time.deltaTime, 0, 0);
     }
-    private void OnTriggerEnter(Collider other)
+     void OnTriggerEnter(Collider other)
     {
-        if(other.tag =="Player")
+        if (other.tag == "Player")
         {
             PlayerManager.numberOfCoins += 1;
             Destroy(gameObject);
-            Debug.Log("Coins:"+ PlayerManager.numberOfCoins);
+            Debug.Log("Coins:" + PlayerManager.numberOfCoins);
+            FindObjectOfType<AudioManager>().PlaySound("coin sound");
+            
         }
+        
     }
+   
+
+
 }
